77fd35eda3794e2c3e09a7fc687eb038eb7175ef
Mohamed Samih <Mohamed.Samih@etu.univ-smb.fr>
2025-12-09 10:09:23 +0100

    Ajoutez des fichiers projet.

---
+++ Crepe_Simulator/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Crepe_Simulator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
